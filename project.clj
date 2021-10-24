02b6c840b1f643b705c32c92fe1862fc0a1a3186
Samiddddd <91602017+Samiddddd@users.noreply.github.com>
2021-10-24 21:51:37 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject chatbot "0.1.0-SNAPSHOT" ;; 1
   :description "This robot will be useful for human"
   ;; The remote repository location in the internets:
-  :url "https://moodle.praguecollege.cz/mod/assign/view.php?id=38425"
+  :url "https://github.com/Samiddddd/Project-Sami-jacob-maxime-"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/%22%7D%22%7D" }
 :dependencies [[org.clojure/clojure "1.10.3"]]
